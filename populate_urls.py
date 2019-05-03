a2e44991e4f13f5cfce1b798877fcf8b3b479ff4
Tom Morrell <tmorrell@caltech.edu>
2019-05-03 09:05:45 -0700

    Working S3 version

--- populate_urls.py
+++ populate_urls.py
@@ -19,23 +19,23 @@ def get_datacite_dois(client_ids):
                 next_link = None
     return linking
 
-def make_s3_record(s3,bucket,resolver_url,url):
+def make_s3_record(s3,bucket,resolver,url):
     '''Make S3 entry for a redirect'''
     s3_object = s3.Object(
     bucket_name=bucket, key=resolver)
-    response = s3_object.put()
-    print(response)
+    response = s3_object.put(WebsiteRedirectLocation=url,ACL='public-read')
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print('Error: ',response)
 
 if __name__ == "__main__":
     client_ids = ['caltech.micropub']
     #['tind.cal','caltech.library','caltech.ipacdoi','caltech.micropub','caltech.hte']
-    #linking = get_datacite_dois(client_ids)
+    linking = get_datacite_dois(client_ids)
     session = boto3.Session(profile_name='resolver')
     current_region = session.region_name
     bucket = 'resolver.library.caltech.edu'
-    s3 = session.client('s3')
-    resolver = 'test'
-    url = 'testurl'
-    make_s3_record(s3,bucket,resolver,url)
-    #result = s3.get_bucket_website(bucket)
-    #print(result)
+    s3 = session.resource('s3')
+    for l in linking:
+        print(l)
+        make_s3_record(s3,bucket,l,linking[l])
+        exit()
